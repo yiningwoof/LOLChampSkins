3d5bcddd1b59447df3d5181d65866a56cdc351da
Yining Wang <ywang@socialsolutions.com>
2022-05-13 00:44:01 -0500

    initialize take quiz logic

---
+++ takeQuiz.js
@@ -0,0 +1,53 @@
+let takeQuizButton = document.getElementById("takeQuizButton");
+let searchContainer = document.getElementById("searchContainer");
+let resultContainer = document.getElementById("resultContainer");
+
+let count = 5;
+
+takeQuizButton.addEventListener("click", loadQuizPage);
+
+function loadQuizPage() {
+	searchContainer.innerHTML = "";
+	resultContainer.innerHTML = "";
+	fetchAllChampions();
+}
+
+function fetchAllChampions() {
+	fetch(
+		"http://ddragon.leagueoflegends.com/cdn/12.9.1/data/en_US/champion.json"
+	)
+		.then((res) => res.json())
+		.then((allData) => {
+			console.log(allData.data);
+			let data = allData.data;
+			let champKeyList = Object.keys(data);
+			let champIndices = generateRandomNumbers(champKeyList.length);
+			let randChampNames = champIndices.map((ind) => champKeyList[ind]);
+			let randChampList = randChampNames.map((n) => data[n]);
+			return randChampList;
+		})
+		.then((list) => getRandomizedSkin(list));
+}
+
+function generateRandomNumbers(max) {
+	let numbers = [];
+	let i = 0;
+	while (i < count) {
+		numbers.push(Math.floor(Math.random() * max));
+		i++;
+	}
+	return numbers;
+}
+
+function getRandomizedSkin(champList) {
+	console.log(champList);
+	champList.forEach((champ) => {
+		// let skinRootUrl = `http://ddragon.leagueoflegends.com/cdn/img/champion/centered/${champName}_${skin.num}.jpg`;
+		fetch(
+			`http://ddragon.leagueoflegends.com/cdn/12.9.1/data/en_US/champion/${champ.id}.json`
+		)
+			.then((res) => res.json())
+			.then((data) => console.log(data.data[champ.id].skins));
+		// let randSkinIds = generateRandomNumbers();
+	});
+}
